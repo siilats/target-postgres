2db3e47eb1aec20c031c947d97cf6623875119d4
Douwe Maan <douwe@gitlab.com>
2020-08-04 12:04:21 -0500

    Bymp version to meltano.1.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="target-postgres",
-    version="meltano.1.1.6",
+    version="meltano.1.1.7",
     description="Meltano maintained fork of target-postgres - Singer.io target for Postgres",
     author="Meltano and Statsbot",
     url="https://statsbot.co",
